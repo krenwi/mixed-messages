c79361d2042d988f89fb4181f9c0c45dba92967f
krenwi <100181516+krenwi@users.noreply.github.com>
2022-06-21 16:08:49 +1200

    Add data for random messages

---
+++ app.js
@@ -0,0 +1,6 @@
+//Data for mixed messages to be randomised from:
+
+const earthTimeUnit = ['first light', 'sunrise', 'music', 'training', 'ablutions', 'noon', 'libations', 'prayer', 'leasure', 'endings', 'sunset', 'evening']
+const moonTimeUnit = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+const sunTimeUnit = ['Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scoropius', 'Ophiuchus', 'Sagittarius']
+
