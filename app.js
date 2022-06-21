2ae9d345cbe6b848edb9e8ffa5050b152c96320f
krenwi <100181516+krenwi@users.noreply.github.com>
2022-06-21 17:13:17 +1200

    Fix random fucntion array length problem

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-//Data for mixed messages to be randomised from:
+//Data for mixed messages to be randomised from: 12 13 12
 
 const earthTimeUnit = ['first light', 'sunrise', 'music', 'training', 'ablutions', 'noon', 'libations', 'prayer', 'leasure', 'endings', 'sunset', 'evening']
-const moonTimeUnit = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 const sunTimeUnit = ['Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scoropius', 'Ophiuchus', 'Sagittarius']
+const moonTimeUnit = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
 
 //Factory function
 const randIndex = (timeUnit) => {
-  console.log(timeUnit.length - 1)
-  return Math.floor(Math.random() * (timeUnit.length - 1))
+  return Math.floor(Math.random() * (timeUnit.length))
 }
 
 const timeGenerator = () => {
