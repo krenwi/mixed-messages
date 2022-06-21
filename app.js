ce9ae5358ad0dfec5a5944886b6283124bcc68cf
krenwi <100181516+krenwi@users.noreply.github.com>
2022-06-21 16:52:47 +1200

    Add basic  message function and call

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ const earthTimeUnit = ['first light', 'sunrise', 'music', 'training', 'ablutions
 const moonTimeUnit = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 const sunTimeUnit = ['Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scoropius', 'Ophiuchus', 'Sagittarius']
 
+const timeGenerator = () => {
+  let rand = Math.floor(Math.random() * 13)
+  console.log(`The hour of the day is ${earthTimeUnit[rand]}.\nWhile the sun is in the constellation ${sunTimeUnit[rand]}.\nAnd the moon is ${moonTimeUnit[rand]}.`)
+}
+
+timeGenerator()
